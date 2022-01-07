5c6476c6da12b4102c70650a9769ca535b17bc26
Benje Dev <Jasminlabidi@gmx.de>
2022-01-07 09:43:38 +0100

    create footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <div className="App">
         <Weather />
       </div>
+      <footer className="footer">
+        <a
+          href="https://github.com/Benje-Dev/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+          className="footer-link"
+        >
+        Open-source code {""}
+        </a>
+         by Jasmin Labidi
+      </footer>
     </div>
   );
 }
