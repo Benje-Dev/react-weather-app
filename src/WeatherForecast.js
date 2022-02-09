1b96937800308b88ab48f1f8fc28c64ed42576d0
Benje Dev <Jasminlabidi@gmx.de>
2022-02-09 13:11:00 +0100

    useEffect refresh forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -7,6 +7,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -19,8 +23,8 @@ export default function WeatherForecast(props) {
         {forecast.map(function (dailyForecast, index) {
           if (index < 5) {
             return (
-              <div className="col-2 forecast-small">
-                <WeatherForecastDay data={dailyForecast} key={index} />
+              <div className="col-2 forecast-small" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
               </div>
             );
           } else {
